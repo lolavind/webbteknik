0bd126e7d917882dd6749013f9fdb694957cdd7a
Per Landberg <per.landberg@polago.se>
2025-02-20 18:08:05 +0100

    Improve script to exit with errorcode if there are issues

--- pa11y-all.mjs
+++ pa11y-all.mjs
@@ -3,10 +3,11 @@ import reporter from 'pa11y/lib/reporters/cli.js';
 import * as path from 'node:path';
 import { glob } from 'node:fs/promises';
 
+var errors = 0;
 async function runPa11y(file) {
   try {
-    console.log('Testing file', file);
     const results = await pa11y(file);
+    errors += results.issues.length;
     const text = await reporter.results(results);
     console.log(text);
   } catch (error) {
@@ -14,6 +15,17 @@ async function runPa11y(file) {
   }
 }
 
-for await (const file of glob('webbteknik/**/*.html')) {
-  runPa11y(file);
+async function runAll() {
+  const all = [];
+  for await (const file of glob('webbteknik/**/*.html')) {
+    all.push(runPa11y(file));
+  }
+  return Promise.all(all);
+}
+
+await runAll();
+
+if (errors > 0) {
+  console.log("Total number of errors: ", errors);
+  process.exit(2);
 }
