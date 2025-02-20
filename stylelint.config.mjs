bc36d4d68f6583c9b3a28f4618f00c7f3a68b86a
Per Landberg <per.landberg@polago.se>
2025-02-20 16:45:05 +0100

    Add script for local dev and testing

---
+++ stylelint.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import('stylelint').Config} */
+export default {
+  defaultSeverity: "warning",
+  extends: ["./css-stylelint-standard.json"] // This is the config used by https://github.com/Webperf-se/webperf_core
+};
