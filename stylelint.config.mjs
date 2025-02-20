0641c219a4da926c86785d2b75afcce58c7081ac
Per Landberg <per.landberg@polago.se>
2025-02-20 22:01:17 +0100

    Switch stylelint config to stylelint-config-standard

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('stylelint').Config} */
 export default {
-  defaultSeverity: "warning",
-  extends: ["./css-stylelint-standard.json"] // This is the config used by https://github.com/Webperf-se/webperf_core
+  defaultSeverity: "error",
+  extends: ["stylelint-config-standard"]
 };
